91a9ffb1591f7da51b0448049f07ad2289715c5b
Ryusuke Sakazaki <sakazaki@tsuzuki.co.jp>
2012-06-14 10:31:25 +0900

    pp

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+
 
 	}
 
