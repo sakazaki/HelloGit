d102d0d84d13dab0be83af5965512e35b34c163c
Ryusuke Sakazaki <sakazaki@tsuzuki.co.jp>
2012-09-27 14:58:41 +0900

    b1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -32,6 +32,10 @@ public class HelloWorld {
 		System.out.println("Java");
 		System.out.println("Java");
 
+		System.out.println("b1");
+		System.out.println("b1");
+		System.out.println("b1");
+
 	}
 
 }
